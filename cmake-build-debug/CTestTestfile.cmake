266fd8e2b0a84b4b091461b028526395f3a4f0d6
osiaeg <egor.goncharov.osia@mail.ru>
2023-04-16 00:58:33 +0300

    first launch

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Egor/CLionProjects/cpp-async
+# Build directory: C:/Users/Egor/CLionProjects/cpp-async/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Egor on 15.04.2023.
+//
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello world!" << std::endl;
+    std::cout << argv[1] << std::endl;
+    return 0;
+}
