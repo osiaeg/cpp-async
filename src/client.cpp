f67fabfb85d98a40aa28513e1ebe70a816589a97
osiaeg <egor.goncharov.osia@mail.ru>
2023-05-14 18:15:20 +0300

    Add threads to client.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -1,50 +1,84 @@
 #include <cstdlib>
+
 #include <cstring>
+
 #include <iostream>
+
 #include "boost/asio.hpp"
+
+#include <thread>
 
 using boost::asio::ip::tcp;
 
-enum { max_length = 1024 };
+enum {
+    max_length = 1024
+};
+const std::string HOST = "localhost";
+const std::string PORT = "9000";
 
-int main(int argc, char* argv[]) {
-  try {
-    if (argc != 3) {
-      std::cerr << "Usage: async_client <num_threads> <read_write_cycles>\n";
-      return 1;
-    }
-
-    int num_threads = std::stoi(argv[1]);
-    std::cout << "Number of clients: " << num_threads << std::endl;
-    int read_write_cycles = std::stoi(argv[2]);
-    std::cout << "Read/Write cycles: " << read_write_cycles << std::endl;
-
-    boost::asio::io_context io_context;
-
-    tcp::socket s(io_context);
-    tcp::resolver resolver(io_context);
-    const std::string HOST = "localhost";
-    const std::string PORT = "9000";
+void connect_client(boost::asio::io_context & context, int cycles) {
+    tcp::socket s(context);
+    tcp::resolver resolver(context);
     boost::asio::connect(s, resolver.resolve(HOST, PORT));
 
-    for (int i = 0; i < read_write_cycles; i++) {
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
+    for (int i = 0; i < cycles; i++) {
+        std::cout << "Enter message: \n";
+        char request[max_length] { 0 };
+        //        std::cin.getline(request, max_length);
         size_t request_length = std::strlen(request);
         boost::asio::write(s, boost::asio::buffer(request, request_length));
 
-        char reply[max_length];
-        size_t reply_length = boost::asio::read(s,
-            boost::asio::buffer(reply, request_length));
+        char reply[max_length] { 0 };
+        size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, request_length));
         std::cout << "Reply is: ";
         std::cout.write(reply, reply_length);
         std::cout << "\n";
     }
-  }
-  catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
 
-  return 0;
+}
+
+int main(int argc, char * argv[]) {
+    try {
+        if (argc != 3) {
+            std::cerr << "Usage: async_client <num_threads> <read_write_cycles>\n";
+            return 1;
+        }
+
+        int num_threads = std::stoi(argv[1]);
+        std::cout << "Number of clients: " << num_threads << std::endl;
+        int read_write_cycles = std::stoi(argv[2]);
+        std::cout << "Read/Write cycles: " << read_write_cycles << std::endl;
+        boost::asio::io_context io_context;
+        std::thread t1 {
+                connect_client,
+                std::ref(io_context),
+                100
+        };
+        std::thread t2 {
+                connect_client,
+                std::ref(io_context),
+                5000
+        };
+        std::thread t3 {
+                connect_client,
+                std::ref(io_context),
+                1000
+        };
+        t1.join();
+        t2.join();
+        t3.join();
+
+        //      std::vector<std::thread> threads(num_threads);
+        //      // spawn n threads:
+        //      for (int i = 0; i < num_threads; i++) {
+        //          threads[i] = std::thread(connect_client, std::ref(io_context), read_write_cycles);
+        //      }
+        //      for (auto& th : threads) {
+        //          th.join();
+        //      }
+    } catch (std::exception & e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,3 @@
-#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <utility>
@@ -6,93 +5,87 @@
 
 using boost::asio::ip::tcp;
 
-class session : public std::enable_shared_from_this<session> {
-public:
-  session(tcp::socket socket, int num) : socket_(std::move(socket)) {
-      this->num_ = num;
-  }
+class session: public std::enable_shared_from_this < session > {
+public: session(tcp::socket socket, int num): socket_(std::move(socket)) {
+        this -> num_ = num;
+    }
 
-  void start() {
-    do_read();
-  }
+    void start() {
+        do_read();
+    }
 
-private:
-  void connection_lost() const {
-    std::cout << "Connection lost: " << num_ << std::endl;
-  }
+private: void connection_lost() const {
+        std::cout << "connection lost: " << num_ << std::endl;
+    }
 
-  void do_read() {
-    auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        [this, self](boost::system::error_code ec, std::size_t length) {
-          if (!ec) {
-            do_write(length);
-          } else {
-              connection_lost();
-          }
-        });
-  }
+    void do_read() {
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                                [this, self](boost::system::error_code ec, std::size_t length) {
+                                    if (!ec) {
+                                        do_write(length);
+                                    } else if (ec == boost::asio::error::eof || ec == boost::asio::error::connection_reset) {
+                                        connection_lost();
+                                    }
+                                });
+    }
 
-  void do_write(std::size_t length) {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-          if (!ec) {
-            do_read();
-          } else {
-              connection_lost();
-          }
-        });
+    void do_write(std::size_t length) {
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+                                 [this, self](boost::system::error_code ec, std::size_t /*length*/ ) {
+                                     if (!ec) {
+                                         do_read();
+                                     }
+                                 });
 
-  }
+    }
 
-  tcp::socket socket_;
-  int num_;
-  enum { max_length = 1024 };
-  char data_[max_length]{};
+    tcp::socket socket_;
+    int num_;
+    enum { max_length = 1024 };
+    char data_[max_length] {};
 };
 
 class server {
-public:
-  server(boost::asio::io_context& io_context, short port) : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
-    do_accept();
-  }
+public: server(boost::asio::io_context & io_context, short port): acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        do_accept();
+    }
 
-private:
-  void do_accept()
-  {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, tcp::socket socket) {
-        
-          if (!ec) {
-              ++client_num;
-            std::shared_ptr<session> Session = std::make_shared<session>(std::move(socket), client_num);
-            Session->start();
-            std::cout << "Connection apply: " << client_num << std::endl;
-          }
+private: void do_accept() {
+        acceptor_.async_accept(
+                [this](boost::system::error_code ec, tcp::socket socket) {
 
-          do_accept();
-        });
-  }
+                    if (!ec) {
+                        ++client_num;
+                        std::shared_ptr < session > Session = std::make_shared < session > (std::move(socket), client_num);
+                        Session -> start();
+                        std::cout << "connection established: " << client_num << std::endl;
+                    }
 
-  int client_num {0};
-  tcp::acceptor acceptor_;
+                    do_accept();
+                });
+    }
+
+    int client_num {
+            0
+    };
+    tcp::acceptor acceptor_;
 };
 
-int main(int argc, char* argv[]) {
-  try {
+int main() {
+    try {
 
-    boost::asio::io_context io_context;
+        boost::asio::io_context io_context;
 
-    short port = 9000;
-    server s(io_context, port);
-    std::cout << "Server start on port: " << port << std::endl;
+        short port = 9000;
+        server s(io_context, port);
+        std::cout << "Server start on port: " << port << std::endl;
 
-    io_context.run();
-  }
-  catch (std::exception& e) {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
+        io_context.run();
+    } catch (std::exception & e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
-  return 0;
+    return 0;
 }
